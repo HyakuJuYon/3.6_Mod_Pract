4977e3ee8a72a96afdf6337a21a1665ddd4d92c4
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-09 23:28:11 +0300

    Changes_1

--- Program.cs
+++ Program.cs
@@ -7,7 +7,13 @@ namespace _3._6_Mod_Pract
         static void Main(string[] args)
         {
             double result = 5.0 / 2.0;
+            
             Console.WriteLine("5 / 2 = {0}", result);
+
+            double result2 = 10 / 4;
+
+            Console.WriteLine("10 / 4 = {0}", result2);
+
         }
     }
 }
