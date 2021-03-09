11c023d6a4eb2b8b881b6e3337fc1e66d16cab35
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-09 23:24:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _3._6_Mod_Pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 5.0 / 2.0;
+            Console.WriteLine("5 / 2 = {0}", result);
+        }
+    }
+}
